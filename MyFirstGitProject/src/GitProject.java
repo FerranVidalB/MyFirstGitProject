d871111f594cf8782a6281f29c323733447e4831
alu20908719v <alu20908719v@ti2-09>
2018-03-14 20:16:55 +0100

    Add show message

--- MyFirstGitProject/src/GitProject.java
+++ MyFirstGitProject/src/GitProject.java
@@ -7,6 +7,11 @@ public class GitProject {
 		super();
 		this.param1 = param1;
 		this.param2 = param2;
+		
+	}
+	private void showMessage() {
+		
+		System.out.println("Message from DavidPrades");
 	}
 	
 	
