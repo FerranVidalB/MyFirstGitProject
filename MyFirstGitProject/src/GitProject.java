a357c2843df2f6dce65cad2a432351349db8b4de
alu20925473g <alu20925473g@ti2-11>
2018-03-14 19:42:40 +0100

    Add Constructor

--- MyFirstGitProject/src/GitProject.java
+++ MyFirstGitProject/src/GitProject.java
@@ -2,4 +2,12 @@
 public class GitProject {
 	private String param1;
 	private String param2;
+	
+	public GitProject(String param1, String param2) {
+		super();
+		this.param1 = param1;
+		this.param2 = param2;
+	}
+	
+	
 }
